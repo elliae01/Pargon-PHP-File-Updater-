fafd6a41b7c4f78310478df88f0141d71343bdfe
aaron elliott <elliae01@pfw.edu>
2018-11-15 04:26:37 +0000

    updated login redirect

--- html/loginadmin.php
+++ html/loginadmin.php
@@ -49,71 +49,7 @@
 
 <body>
 	
-	<?php
-    /**
-     * 
-     *
-     * A simple login module that lets a user stay logged in
-     * by saving username and, ack, password in cookies.
-     *
-     * Refer to CS75 (David J. Malan)
-     */
-
-    // enable sessions
-    session_start();
-
-    // were this not a demo, these would be in some database
-    define("USER", "cs372");
-    define("PASS", "pfw");
-
-    // if username and password were saved in cookie, check them
-    if (isset($_COOKIE["user"]) && isset($_COOKIE["pass"]))
-    {
-        // if username and password are valid, log user back in
-        if ($_COOKIE["user"] == USER && $_COOKIE["pass"] == PASS)
-        {
-            // remember that user's logged in
-            $_SESSION["authenticated"] = true;
-
-            // re-save username and, ack, password in cookies for another week
-            setcookie("user", $_COOKIE["user"], time() + 7 * 24 * 60 * 60);
-            setcookie("pass", $_COOKIE["pass"], time() + 7 * 24 * 60 * 60);
-
-            // redirect user to home page, using absolute path, per
-            // http://us2.php.net/manual/en/function.header.php
-            $host = $_SERVER["HTTP_HOST"];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header("Location: portal.html");
-            exit;
-        }
-    }
-
-    // else if username and password were submitted, grab them instead
-    else if (isset($_POST["user"]) && isset($_POST["pass"]))
-    {
-        // if username and password are valid, log user in
-        if ($_POST["user"] == USER && $_POST["pass"] == PASS)
-        {
-            // remember that user's logged in
-            $_SESSION["authenticated"] = true;
-
-            // save username in cookie for a week
-            setcookie("user", $_POST["user"], time() + 7 * 24 * 60 * 60);
-
-            // save password in, ack, cookie for a week if requested
-            if ($_POST["keep"])
-                setcookie("pass", $_POST["pass"], time() + 7 * 24 * 60 * 60);
-
-            // redirect user to home page, using absolute path, per
-            // http://us2.php.net/manual/en/function.header.php
-            $host = $_SERVER['HTTP_HOST'];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header("Location: http://$host$path/home.php");
-            exit;
-        }
-    }
-?>
-
+	<?php include '../php/login.php';?>
 	
 	
 	<div id="centerpage" style="width: 40%">
