9920fc0139e9440640561fc5a4c372c2a9eb8eee
Mark Erickson <ericmw02@pfw.edu>
2018-11-26 04:05:07 +0000

    added sample insert function

--- php/SampleQuery-2.php
+++ php/SampleQuery-2.php
@@ -5,7 +5,11 @@
 */
 	require_once('../includes/dbHelper.php');
    $db=dbConnect();
-	$sql="select s.name as Software, m.type as Type, c.File_rule, c.Source_folder, c.Destination_folder, c.Notes from software s  join machine m  join controlled_item c where c.Software=s.id and m.id=c.Machine_Type group BY `s`.`name` ASC";
+   
+   $sql="INSERT INTO `controlled_item`(`Software`, `Machine_Type`, `File_rule`, `Source_folder`, `Destination_folder`, `Notes`) VALUES (2,1,'*.txt','C:\\\Source','C:\\\Destination',CONCAT('Sample Insert at ',cast(Now() as char)))";
+   SendSQLCMD($db, $sql);
+	
+	$sql="select s.name as Software, m.type as Type, c.File_rule, c.Source_folder, c.Destination_folder, c.Notes from software s  join machine m  join controlled_item c where c.Software=s.id and m.id=c.Machine_Type";
 	DisplayDBasTable($db,$sql,false);
 	$db->close();
 	unset($db);
