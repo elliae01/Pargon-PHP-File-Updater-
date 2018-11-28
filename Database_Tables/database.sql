c6774f8beed647d03914277080bfcbc6c7b1ab28
Mark Erickson <ericmw02@pfw.edu>
2018-11-28 21:28:11 +0000

    displaying actual info stored in the db

--- Database_Tables/database.sql
+++ Database_Tables/database.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2018 at 04:49 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Generation Time: Nov 28, 2018 at 09:13 PM
+-- Server version: 5.5.57-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `database`
@@ -28,20 +26,27 @@ SET time_zone = "+00:00";
 -- Table structure for table `computername`
 --
 
-CREATE TABLE `computername` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `computername` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `assetID` varchar(10) NOT NULL,
   `IP_wired` varchar(15) NOT NULL,
-  `IP_wifi` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `IP_wifi` varchar(15) NOT NULL,
+  `usename` varchar(25) NOT NULL,
+  `logonserver` varchar(25) NOT NULL,
+  `lastlogon` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `assetID_INDEX` (`assetID`) USING BTREE,
+  KEY `IP_wired` (`IP_wired`),
+  KEY `IP_wifi` (`IP_wifi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `computername`
 --
 
-INSERT INTO `computername` (`id`, `assetID`, `IP_wired`, `IP_wifi`) VALUES
-(1, 'IT101332', '10.10.10.32', '10.10.10.33'),
-(2, 'IT101333', '10.10.10.10', '10.10.10.11');
+INSERT INTO `computername` (`id`, `assetID`, `IP_wired`, `IP_wifi`, `usename`, `logonserver`, `lastlogon`) VALUES
+(1, 'IT101332', '10.10.10.32', '10.10.10.33', 'marke', '\\\\YOGA14', '0000-00-00 00:00:00'),
+(2, 'IT101333', '10.10.10.10', '10.10.10.11', 'ljoh01', '\\\\FILESRV01HQ', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -49,23 +54,32 @@ INSERT INTO `computername` (`id`, `assetID`, `IP_wired`, `IP_wifi`) VALUES
 -- Table structure for table `controlled_item`
 --
 
-CREATE TABLE `controlled_item` (
-  `id` int(11) NOT NULL,
-  `Software` varchar(30) NOT NULL,
-  `Machine_Type` varchar(15) NOT NULL,
+CREATE TABLE IF NOT EXISTS `controlled_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Software` int(15) NOT NULL,
+  `Machine_Type` int(15) NOT NULL,
   `File_rule` varchar(256) NOT NULL,
   `Source_folder` varchar(500) NOT NULL,
   `Destination_folder` varchar(500) NOT NULL,
-  `Notes` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Notes` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Dumping data for table `controlled_item`
 --
 
 INSERT INTO `controlled_item` (`id`, `Software`, `Machine_Type`, `File_rule`, `Source_folder`, `Destination_folder`, `Notes`) VALUES
-(1, '1', '1', '*.pst', 'F:\\Engineering\\Post Processers\\Matercam\\Paragon_Post_Processors\\Doosan21i-Indy', 'C:\\Users\\Public\\Documents\\shared Mcam2019\\mill\\Posts', 'Notes Here'),
-(2, '5', '2', '8', 'f:', 'c:', '');
+(1, 1, 1, '*.pst', 'F:\\Engineering\\Post Processers\\Matercam\\Paragon_Post_Processors\\Doosan21i-Indy', 'C:\\Users\\Public\\Documents\\shared Mcam2019\\mill\\Posts', 'Notes Here'),
+(2, 5, 2, '8', 'f:', 'c:', ''),
+(17, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert 2018-11-26 04:03:43'),
+(18, 2, 1, '*.hdr', 'C:\\Source', 'C:\\Destination', 'Sample Insert at2018-11-26 04:04:16'),
+(19, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-26 16:54:00'),
+(20, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-26 16:55:01'),
+(21, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-26 16:55:05'),
+(22, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-28 16:38:37'),
+(23, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-28 16:38:44'),
+(24, 2, 1, '*.txt', 'C:\\Source', 'C:\\Destination', 'Sample Insert at 2018-11-28 16:38:45');
 
 -- --------------------------------------------------------
 
@@ -73,11 +87,39 @@ INSERT INTO `controlled_item` (`id`, `Software`, `Machine_Type`, `File_rule`, `S
 -- Table structure for table `logintracking`
 --
 
-CREATE TABLE `logintracking` (
+CREATE TABLE IF NOT EXISTS `logintracking` (
   `Employee_ID` varchar(40) NOT NULL,
   `Date_Time` varchar(40) NOT NULL,
-  `Login_Count` int(40) NOT NULL
+  `Login_Count` int(40) NOT NULL,
+  PRIMARY KEY (`Employee_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `LineID` int(11) NOT NULL AUTO_INCREMENT,
+  `LogID` varchar(225) NOT NULL,
+  `Time` datetime DEFAULT NULL,
+  `Computer` varchar(100) DEFAULT NULL,
+  `IP` varchar(35) DEFAULT NULL,
+  `User` varchar(40) DEFAULT NULL,
+  `Action` varchar(500) DEFAULT NULL,
+  `File` varchar(500) DEFAULT NULL,
+  `Success` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`LineID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`LineID`, `LogID`, `Time`, `Computer`, `IP`, `User`, `Action`, `File`, `Success`) VALUES
+(1, '21.35F', '2018-09-09 14:56:27', 'IT101348', '10.128.6.71', 'kful01', 'Started Update', NULL, 'Success'),
+(2, '21.35F', '2018-09-09 14:56:43', 'IT101348', '10.128.6.71', 'kful01', 'Completed Update from \\\\NETWORKSRV04HQ ', NULL, 'Success');
 
 -- --------------------------------------------------------
 
@@ -85,10 +127,11 @@ CREATE TABLE `logintracking` (
 -- Table structure for table `machine`
 --
 
-CREATE TABLE `machine` (
-  `id` int(15) NOT NULL,
-  `type` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `machine` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `type` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `machine`
@@ -109,16 +152,17 @@ INSERT INTO `machine` (`id`, `type`) VALUES
 -- Table structure for table `pc_status`
 --
 
-CREATE TABLE `pc_status` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `pc_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Date` date NOT NULL,
   `Time` time NOT NULL,
   `Computer` varchar(10) NOT NULL,
   `IP_wifi` varchar(15) NOT NULL,
   `IP_wired` varchar(15) NOT NULL,
   `user` varchar(8) NOT NULL,
-  `Notes` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Notes` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -126,10 +170,12 @@ CREATE TABLE `pc_status` (
 -- Table structure for table `software`
 --
 
-CREATE TABLE `software` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `software` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `software`
@@ -151,97 +197,13 @@ INSERT INTO `software` (`id`, `name`) VALUES
 -- Table structure for table `user_and_password`
 --
 
-CREATE TABLE `user_and_password` (
+CREATE TABLE IF NOT EXISTS `user_and_password` (
   `Employee_ID` varchar(50) NOT NULL,
   `Username` varchar(30) NOT NULL,
   `Password` varchar(30) NOT NULL,
-  `Email` varchar(30) NOT NULL
+  `Email` varchar(30) NOT NULL,
+  PRIMARY KEY (`Employee_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `computername`
---
-ALTER TABLE `computername`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `assetID_INDEX` (`assetID`) USING BTREE,
-  ADD KEY `IP_wired` (`IP_wired`),
-  ADD KEY `IP_wifi` (`IP_wifi`);
-
---
--- Indexes for table `controlled_item`
---
-ALTER TABLE `controlled_item`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `logintracking`
---
-ALTER TABLE `logintracking`
-  ADD PRIMARY KEY (`Employee_ID`);
-
---
--- Indexes for table `machine`
---
-ALTER TABLE `machine`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pc_status`
---
-ALTER TABLE `pc_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `software`
---
-ALTER TABLE `software`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
-
---
--- Indexes for table `user_and_password`
---
-ALTER TABLE `user_and_password`
-  ADD PRIMARY KEY (`Employee_ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `computername`
---
-ALTER TABLE `computername`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `controlled_item`
---
-ALTER TABLE `controlled_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `machine`
---
-ALTER TABLE `machine`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `pc_status`
---
-ALTER TABLE `pc_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `software`
---
-ALTER TABLE `software`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- html/administeritems.php
+++ html/administeritems.php
@@ -1,3 +1,8 @@
+<?php
+	require_once('../includes/dbHelper.php');
+   $db=dbConnect();
+
+?>
 <!DOCTYPE html>
 <!-- This example is based on the examle in "Dynamic Web Programming and HTML5" by Paul S. Wang -->
 
@@ -5,9 +10,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Controlled Items Administer Items</title>
-	<link rel="stylesheet" href="../css/floatlayout.css" 
+	<link rel="stylesheet" href="../../css/floatlayout.css" 
 		type="text/css" title="float layout style">
-	<script type="text/javascript" src="../js/administeritems.js"></script>
+	<script type="text/javascript" src="../../js/administeritems.js"></script>
 	<style type="text/css">
 		body {
 			margin: 50px; }
@@ -19,18 +24,14 @@
 </head>
 
 <body>
-	
-<?php include '../php/login.php';?>
-<?php if($_SESSION["authenticated"] == true):?>
-
 	<div id="centerpage">
 		<header class="banner">
-		<img src="../images/nninc.jpg" alt="Paragons Logo" class="center"></img>
+		<img src="../../images/nninc.jpg" alt="Paragons Logo" class="center"></img>
 			<section class="logo">Administer List of Controlled Items</section>
 			<nav>         
-				<a href="./portal.php">Return to Portal</a>
-				<a href="./additem.php">Add new Item</a>
-				<a href="./reviewItems.php">Review Item</a>
+				<a href="../../html/portal.html">Return to Portal</a>
+				<a href="../../html/additem.html">Add new Item</a>
+				<a href="../../html/reviewItems.html">Review Item</a>
 			</nav>
 		</header>
 		
@@ -54,8 +55,15 @@
 				<option>Lathe</option>
 				<option>Wire</option>
 			</select>
+			
+			<?php
+				$sql="select s.name as Software, m.type as 'Machine Type', c.File_rule as 'File, Folder, or Rule', c.Source_folder as 'Source Folder', c.Destination_folder as 'Destination Folder', c.Notes from software s  join machine m  join controlled_item c where c.Software=s.id and m.id=c.Machine_Type";
+				DisplayDBasTable($db,$sql,false);
+				$db->close();
+				unset($db);
+			?>
 			<table>
-				<script src="../js/admintable.js"></script>
+				<script src="../../js/admintable.js"></script>
 			</table>
 			</article>
 			<!-- Main Content End -->
@@ -63,7 +71,7 @@
 
 			<aside>
 				<h1>Reference Links</h1>
-				<embed  id="mysidebar" src="sidebar.html">
+				<embed  id="mysidebar" src="../../html/sidebar.html">
 			</aside>
 			<!-- Right Box End-->
 
@@ -74,12 +82,6 @@
 	<footer>
 		<p style="text-align: center;"></p>
 	</footer>
-
-<?php else : ?>
-	<font size="10px" color="red">Access Denied!</font><br><br>
-	<font size="4px" color="blue">Click here to </font>
-	<a href="../php/loginSession.php">Login</a>
-<?php endif; ?>
-
+	
 </body>
 </html>
