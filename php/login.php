a9491f8d71e11b5c680da19667e048b20f40a2ff
aaron elliott <elliae01@pfw.edu>
2018-11-28 04:00:10 +0000

    Added login protection to all pages, changed file extensions to php

--- php/login.php
+++ php/login.php
@@ -1,16 +1,15 @@
 <?php
-    /**
-     * 
-     *
-     * A simple login module that lets a user stay logged in
-     * by saving username and, ack, password in cookies.
-     *
-     * Refer to CS75 (David J. Malan)
-     */
 
-    // enable sessions
-    session_start();
     
+    // enable sessions
+    //session_start();
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    else{
+        session_destroy();
+        $_SESSION["authenticated"] == false;
+    }
     
     
     
@@ -37,7 +36,7 @@
             $_SESSION["authenticated"] = true;
             
             // save username in cookie for a week
-            setcookie("pass", $_POST["pass"], time() + 7 * 24 * 60 * 60);
+            setcookie("user", $_POST["user"], time() + 7 * 24 * 60 * 60);
             //setcookie("pass", $_POST["pass"], time()+10);
             
             // save password in, ack, cookie for a week if requested
@@ -47,11 +46,8 @@
                 setcookie("user", $_POST["user"], time() + 7 * 24 * 60 * 60);
             }
 
-            // redirect user to home page, using absolute path, per
-            // http://us2.php.net/manual/en/function.header.php
-            $host = $_SERVER['HTTP_HOST'];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header("Location: portal.html");
+            // redirect user
+            header("Location: portal.php");
             exit;
         }else{
             $error1= "<div style ='font:18px Arial,tahoma,
